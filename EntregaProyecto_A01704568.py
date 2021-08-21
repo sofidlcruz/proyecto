7698c3731a22059f85875cf876b68f0bccdb3fcc
sofidlcruz <88898500+sofidlcruz@users.noreply.github.com>
2021-08-20 19:49:00 -0500

    Avance 1 proyecto

---
+++ EntregaProyecto_A01704568.py
@@ -0,0 +1,39 @@
+"""
+Sofía de la Cruz Vázquez - A01704568
+
+Examen sobre turismo en México
+
+Tema= Turismo en México
+Contexto= Considero que conocer lugares turísticos de nuestro país es un tema
+muy interesante por la recreación didáctica de viajar y relacionar con la
+historia del país. Además, valoro es un tema necesario de reforzar en la cultura
+general para conocer más del territorio mexicano. Es de gran interés
+conocer estos destinos ya sea para disfrutarlos o conocer más acerca
+del patrimonio de México. Tomar este examen es una manera dinámica de evaluar
+el conocimiento general que se tiene de estos lugares además de conocer
+nuevos lugares turísticos que pueden ser de interés para el usuario.
+
+Algoritmo:
+
+El proyecto se trata de un examen sobre algunos de los sitios turísticos de México
+donde se harán preguntas de conocimiento cultural sobre pueblos mágicos, atracciones
+regionales, playas y museos.
+R = Respuesta correcta
+
+EI (resultados preguntas)
+
+El usuario se identificará con su nombre y contestará el examen según los datos requeridos
+en preguntas de opción múltiple o cerrada.
+Algunas de las preguntas planeadas son:
+P1- Estado en donde se encuentra Zona Arqueológica de Monte Albán. R= Oaxaca
+P2- Pueblo mágico conocido por su arquitectura barroca. Se encuentra la Parroquia de San Miguel Arcángel. R= San Miguel de Allende
+P3-¿Cuál es la capital de Nuevo León? R = Monterrey
+P4-¿Cavernas con agua que eran sitios sagrados para los mayas? R= Cenotes
+P5-¿Medio de transporte turístico que recorre la Sierra de México? R= El chepe
+P6-Uno de los cañones más importantes en el mundo con gran riqueza ecológica. Ubicado en el sur de México. R= Cañón del sumidero  
+P7-Es uno de los pueblos magicos de Puebla que se destaca por la producción de sidra, pirotecnia, alfarería. R= Cholula
+P8-¿Dónde se localizan las Salina con mayor concentración de borrego cimarrón del mundo? r=Baja california
+P9- Formación rocosa de un arco natural ubicado en los Cabos. R= El Arco de Cabo San Lucas.
+
+EF (mostrar resultado final, mostrar el número de las pregunta(s) erronea(s))
+"""
